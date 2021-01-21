4fa78e965aa175fba9dc630922dc1d16f603a0dd
Albina <alninayakupova9@icloud.com>
2021-01-22 00:56:51 +0300

    Выполнено, программа работает.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,38 @@ namespace DZ_2._2
 {
     class Program
     {
+        enum Months
+        {
+            Январь = 1,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Для того чтобы определить текущий месяц, прошу ввести порядковый его номер с 1 до 12 :");
+                Months months;
+                int M = int.Parse(Console.ReadLine());         
+                months = (Months)M;
+            Console.WriteLine($"Текущий месяц {months}");
+
+         }
+          
+
+
         }
+
     }
 }
+
+
